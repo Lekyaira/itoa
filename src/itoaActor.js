3662fa6d3c6781e741d1f61c9046310f427bc020
Ryan Anderson <anderson.ryan.w@live.com>
2023-04-25 03:48:25 -0500

    Design skills tab, functionality

--- src/itoaActor.js
+++ src/itoaActor.js
@@ -1,13 +1,83 @@
 export default class itoaActor extends Actor {
+    // Item types
+    skills = [];
+    equipment = [];
+
     derived = {
         armor: 1,
         shield: {
             hardness: 0,
             hp: 5
+        },
+        weight: {
+            stones: 0,
+            light: 0,
+            negligible: 0
         }
     };
     
     prepareDerivedData() {
         const data = this.system;
+
+        // Initialize derived data
+        this.derived = {
+            armor: 0,
+            shield: {
+                hardness: 0,
+                hp: 0
+            },
+            weight: {
+                stones: 0,
+                light: 0,
+                negligible: 0
+            }
+        };
+
+        // Populate items lists
+        this.prepareItems(this.items);
+
+
+    }
+
+    prepareItems(items) {
+        this.skills = [];
+        this.equipment = [];
+
+        items.forEach(item => {
+            if(item.type === 'skill') {
+                this.skills.push({item, expanded: false});
+            }
+            if(item.type === 'item') {
+                this.equipment.push({item, expanded: false});
+                // wornState:
+                // 0 = dropped
+                // 1 = stowed
+                // 2 = worn
+                // 3 = 1-hand
+                // 4 = 2-hand
+                // Item is equipped, modify stats
+                if(item.equipable && item.wornState > 1) {
+                    if(item.isArmor) this.derived.armor = Math.max(this.derived.armor, item.armor);
+                    if(item.isShield) {
+                        this.derived.shield.hardness = item.shield.hardness;
+                        this.derived.shield.hp = item.shield.hp;
+                    }
+                    // TODO: Speed modifier
+                    // TODO: Dodge modifier
+                    // TODO: If required strength < strength, add disadvantage
+                }
+
+                // Update carry weight
+                if(item.weightType === 0) { // Stones
+                    this.derived.weight.stones += item.weight;
+                }
+                else if(item.weightType === 1) { // Light
+                    this.derived.weight.light += item.weight;
+                }
+                else if(item.weightType === 2) { // Negligible
+                    this.derived.weight.negligible += item.weight;
+                }
+            }
+        });
     }
 }
--- src/sheets/SkillsTab.svelte
+++ src/sheets/SkillsTab.svelte
@@ -1,15 +1,78 @@
 <script>
+    import { TJSDocument } from '@typhonjs-fvtt/runtime/svelte/store';
+
     // Sheet and Actor data passed in from base sheet.
-   export let sheet;
-   export let doc;
-   // Set actor variable so we can refer to it later.
-   const actor = doc;
+    export let sheet;
+    export let doc;
+    // Set actor variable so we can refer to it later.
+    const actor = doc;
+
+    ////////////////////////////////
+    // Handle Inventory Functions //
+    ////////////////////////////////
+    async function addNewItem(type)
+    {
+        //const type = event.target.attributes.name.value;
+        console.log(`ITOA | addNewItem, type: '${type}'`);
+        // TODO: Add some cool item adding code here!
+    }
+
+    async function deleteItem(item)
+    {
+        console.log(item);
+        await Item.deleteDocuments([item.id], { parent: $actor });
+    }
+
+    async function editItem(item)
+    {
+        item.sheet.render(true);
+    }
+
+    /**
+    * Handles parsing the drop event and sets the new `uuid` or undefined.
+    *
+    * @param {DragEvent}   event -
+    */
+    async function onDrop(event)
+    {
+            console.log(event);
+            const dropped = new TJSDocument();
+            let droppedSkill;
+            try
+            {
+                await dropped.setFromDataTransfer(JSON.parse(event.dataTransfer.getData('text/plain')))
+                dropped.subscribe(value => droppedSkill = value);
+            }
+            catch (err) { /**/ }
+
+            console.log(droppedSkill);
+
+            if ( !$actor.isOwner ) return false;
+            const data = [{name: droppedSkill.name, type: droppedSkill.type, img: droppedSkill.img, system:{...droppedSkill.system}}];
+            console.log(data);
+            const item = await Item.createDocuments(data, {parent: $actor});
+            console.log(item);
+    }
 </script>
 
 <!-- This is necessary for Svelte to generate accessors TRL can access for `elementRoot` -->
 <svelte:options accessors={true}/>
 
-<div>Skills</div>
+<div on:drop|preventDefault|stopPropagation={onDrop}>
+    <div id="skillsHeader">
+        <i id="addNew" class="editButton fas fa-plus" name="skill" on:click={e => addNewItem("skill")} />
+    </div>
+    {#each $actor.skills as skill}
+    <div class="skillEntry">
+        <img class="skillImage" src="{skill.item.img}" alt="{skill.item.name} image." />
+        <div class="skillTitle" on:click={e => item.expanded = !item.expanded}>{skill.item.name}</div>
+        <div class="editBlock">
+            <i id="editItem" class="editButton fas fa-pen-to-square" on:click={e => editItem(skill.item)} />
+            <i id="deleteItem" class="editButton fas fa-trash" on:click={e => deleteItem(skill.item)} />
+        </div>
+    </div>
+    {/each}
+</div>
 
 <style lang="scss">
     
